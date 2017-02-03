4e627b47de720b1b51a90ddcbfb5eff018f242fe
Einar W. Høst <einarwh@gmail.com>
2017-02-03 18:14:00 +0100

    survive null maybe

--- Lambda/Lambda.fs
+++ Lambda/Lambda.fs
@@ -29,12 +29,17 @@ let what p str =
       ":("
 
 let rec repl () =
-    what expParser (Console.ReadLine ()) |> Console.WriteLine
+    let s = Console.ReadLine()
+    match s with 
+    | null -> Console.WriteLine("?")
+    | s' -> 
+      s' |> what expParser |> Console.WriteLine
     repl ()
 
 [<EntryPoint>]
 let main argv =
-
+    Console.InputEncoding <- System.Text.Encoding.UTF8;
+    Console.OutputEncoding <- System.Text.Encoding.UTF8;
     test expParser "λf.λx.f (f (f (f x)))"
     test expParser "λn.λf.λx.f (n f x)"
     test expParser "λf.λx.f x y z"
@@ -42,6 +47,13 @@ let main argv =
 
     let zeroStr = "λf.λx.x"
     let succStr = "λn.λf.λx.f (n f x)"
+
+    // zero (\f.\x.x)
+    // 2 (\f.\x.f (f x))
+    // 3 (\f.\x.f (f (f x)))
+
+    // succ \n.\f.\x.f (n f x)
+    // add \a.\b.a (\n.\f.\x.f (n f x)) b
 
     let idStr = "λx.x"
     let idIdStr = sprintf "%s %s" idStr idStr
