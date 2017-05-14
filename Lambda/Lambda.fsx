e8f371c15e0b62a05e1ee53663551a4a386b4c9d
Jonas Winje <jonas.winje@gmail.com>
2017-05-14 12:26:40 +0200

    stepwiser eval in fsx

--- Lambda/Lambda.fsx
+++ Lambda/Lambda.fsx
@@ -3,35 +3,13 @@
 #r @"FParsec.dll"
 #r @"FParsecCS.dll"
 
+open System
+open FParsec
+
 type Exp =
   | Var of string
   | Lam of string * Exp
   | App of Exp * Exp
-
-open System
-open FParsec
-
-let expParser, expParserRef = createParserForwardedToRef<Exp, unit>()
-
-let varNameParser = 
-  many1 lower |>> (fun cs -> new String(List.toArray(cs)))
-
-let varParser = varNameParser |>> Var
-let lamParser = 
-        pipe2 ((pchar 'λ' <|> pchar '\\') >>. varNameParser) 
-              (pchar '.' >>. expParser)
-              (fun s e -> Lam (s, e))
-let parParser = between (pchar '(') (pchar ')') expParser
-let notAppParser = lamParser <|> varParser <|> parParser
-
-let appParser = 
-  let applify defs =
-    let leftest = List.head defs
-    let restest = List.tail defs
-    List.fold (fun acc e -> App (acc, e)) leftest restest 
-  sepBy1 notAppParser (pchar ' ') |>> applify
-
-do expParserRef := attempt appParser <|> notAppParser
 
 let unparse =
     let pstr s = "(" + s + ")"
@@ -45,28 +23,58 @@ let unparse =
         | t -> pstr (unparse t)
     unparse
 
+let expParser, expParserRef = createParserForwardedToRef<Exp, unit>()
+
+let varNameParser : Parser<String, unit> = 
+  many1 lower |>> (fun cs -> new String(List.toArray(cs)))
+
+let varParser = varNameParser |>> Var
+
+let lamParser = 
+        pipe2 ((pchar 'λ' <|> pchar '\\') >>. varNameParser) 
+              (pchar '.' >>. expParser)
+              (fun s e -> Lam (s, e))
+
+let parParser = between (pchar '(') (pchar ')') expParser
+
+let notAppParser = lamParser <|> varParser <|> parParser
+
+let appParser = 
+  let applify defs =
+    let leftest = List.head defs
+    let restest = List.tail defs
+    List.fold (fun acc e -> App (acc, e)) leftest restest 
+  sepBy1 notAppParser (pchar ' ') |>> applify
+
+do expParserRef := attempt appParser <|> notAppParser
+
 let rec subst = function
-    | t, s, App (f, a) -> App (subst (t, s, f), subst (t, s, a))
-    | t, s, Lam (p, b) ->
+    | x, s, App (f, a) -> App (subst (x, s, f), subst (x, s, a))
+    | x, s, Lam (p, b) ->
         if p = s
         then Lam (p, b)
-        else Lam (p, subst (t, s, b))
-    | t, s, Var v ->
+        else Lam (p, subst (x, s, b))
+    | x, s, Var v ->
         if v = s
-        then t
+        then x
         else Var v
 
-let rec reduce = function
-    | App (Lam (p, b), a) -> subst (a, p, b)
-    | App (f, a) -> App (reduce f, reduce a)
-    | Lam (p, b) -> Lam (p, reduce b)
-    | Var v -> Var v
+type EvalResult = Next of Exp | Normal
 
-let rec reduceAll t =
-    let res = reduce t
-    if res = t
-    then res
-    else reduceAll res
+let rec reduce = function
+    | Var v -> Normal
+    | App (Lam (p, b), a) -> Next (subst (a, p, b))
+    | App (f, a) ->
+        match reduce f with
+            | Next rf -> Next (App (rf, a))
+            | _ ->
+                match reduce a with
+                    | Next ra -> Next (App (f, ra))
+                    | _ -> Normal
+    | Lam (p, b) ->
+        match reduce b with
+            | Next b -> Next (Lam (p, b))
+            | _ -> Normal
 
 let test p str =
     printfn "%s -> " str 
@@ -77,22 +85,29 @@ let test p str =
 let testReduce p str =
     printfn "%s -> " str 
     match run p str with
-    | Success(result, _, _)   -> printfn " %A" (result |> reduceAll)
+    | Success(result, _, _)   -> printfn " %A" (result |> reduce)
     | Failure(errorMsg, _, _) -> printfn " %s" errorMsg
+
+let rec runEval x =
+    match reduce x with
+        | Normal -> Console.WriteLine (unparse x)
+        | Next nx ->
+            Console.WriteLine (unparse nx)
+            runEval nx
 
 let what p str = 
     match run p str with
     | Success(result, _, _)   -> 
-      result |> reduceAll |> unparse
+      runEval result
     | Failure(errorMsg, _, _) -> 
-      ":("
+      Console.WriteLine ":("
 
-let lambda = what expParser
-
-let rec repl () =
+let rec repl () : unit =
     let s = Console.ReadLine()
     match s with 
     | null -> Console.WriteLine("?")
     | s' -> 
       s' |> what expParser |> Console.WriteLine
     repl ()
+
+repl ()
