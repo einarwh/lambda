efadb47cfefb4dfc028056dd17d09de831f4422d
Einar W. Høst <einarwh@gmail.com>
2017-06-15 16:14:44 +0200

    ndcoslo

--- Lambda/Ndcoslo.fsx
+++ Lambda/Ndcoslo.fsx
@@ -10,6 +10,8 @@ open FParsec
    type for expressions
 *)
 
+type Exp = Var of string | App of Exp * Exp | Lam of string * Exp
+
 (* 
     how to parse some string into some Exp?
 
@@ -18,7 +20,14 @@ open FParsec
 
 (*  helper function for applying some parser to a string *)
 
+let parse p s = 
+  match run p s with 
+  | Success (e, _, _) -> e
+  | Failure (m, _, _) -> failwith m
+
 (*  a recursive parser for expressions *)
+
+let expParser, expParserRef = createParserForwardedToRef<Exp, unit>()
 
 (*
     simple cases: not applications
@@ -28,11 +37,26 @@ open FParsec
     Var of string 
 *)
 
+let varNameParser : Parser<string, unit> = 
+  many1 (lower <|> upper) |>> (List.toArray >> String)
+
+let varParser = 
+  varNameParser |>> Var
+
+expParserRef := varParser
+
 parse expParser "foo" = Var "foo"
 
 (*
     Lam of string * Exp
 *)
+
+let lamParser =
+  let p1 = (pchar 'λ') >>. varNameParser
+  let p2 = (pchar '.') >>. expParser
+  pipe2 p1 p2 (fun name body -> Lam (name, body))
+
+expParserRef := lamParser <|> varParser
 
 parse expParser "λx.x" = Lam ("x", Var "x")
 parse expParser "λa.λb.a" = Lam ("a", Lam ("b", Var "a"))
@@ -41,7 +65,15 @@ parse expParser "λa.λb.a" = Lam ("a", Lam ("b", Var "a"))
     Parentheses for grouping
 *)
 
-parse expParser "(λx.x)" = Lam ("x", Var "x")
+let parParser =
+  between (pchar '(') (pchar ')') expParser
+
+let notAppParser =
+  parParser <|> lamParser <|> varParser
+
+expParserRef := notAppParser
+
+parse expParser "(((λx.x)))" = Lam ("x", Var "x")
 
 (*
     slightly ickier but not very icky
@@ -53,8 +85,15 @@ parse expParser "(λx.x)" = Lam ("x", Var "x")
     parse Exp -> parse App (Exp, Exp) -> parse Exp -> parse App (Exp, Exp)...
 *)
 
+let appParser =
+  chainl1 notAppParser (pchar ' ' |>> (fun _ f a -> App (f, a)))
+
+expParserRef := appParser
+
 parse expParser "x x" = App (Var "x", Var "x")
 parse expParser "a b c" = App (App (Var "a", Var "b"), Var "c")
+
+parse expParser "a (b c)"
 
 let appExpString = "(λa.λb.a b a) (λx.λy.x) foo"
 
@@ -85,98 +124,63 @@ paren "-:   :-"
     if the argument in a function application is
         a lambda or a function application
 *)
-
 let rec unparse = function
+    | Var v -> v
     | Lam (p, x) -> "λ" + p + "." + unparse x
-    | Var s -> s
     | App (Lam (p, x), a) -> paren (unparse (Lam (p, x))) + " " + argstring a
     | App (f, a) -> unparse f + " " + argstring a
 and argstring = function
-    | Var s -> s
+    | Var v -> v
     | x -> paren (unparse x)
 
-
-unparse lamExp1
-unparse lamExp2
-unparse appExp
-
-unparse appExp = appExpString
-
-
-
-
-
-(*
-    subst (arg : Exp, s : String, x : Exp) : Exp
-    (Exp * string * Exp -> Exp)
-    look for *free* occurences of variable s within x
-        and replace them with arg
-*)
-
 let rec subst = function
-    | arg, s, Var v ->
-        if v = s then arg else Var v
-    | arg, s, Lam (p, x) ->
-        if p = s
-        then Lam (p, x)
-        else Lam (p, subst (arg, s, x))
-    | arg, s, App (f, a) ->
-        App (subst (arg, s, f), subst (arg, s, a))
+| arg, s, Var v -> if v = s then arg else Var v
+| arg, s, App (f, a) -> App (subst (arg, s, f), subst (arg, s, a))
+| arg, s, Lam (p, x) -> if p = s then Lam (p, x) else Lam (p, subst (arg, s, x)) 
 
-
-appExpString
-
-unparse (subst (Var "horse", "foo", appExp))
-unparse (subst (Var "horse", "x", appExp))
-
-
-(*
-    reduce (x : Exp) : EvalResult
-    (Exp -> EvalResult)
-    look for a function applications where the function is a lambda
-
-    if we find one: return a Next
-        with the body of the lambda
-            with function argument substituted for the lambda-parameter
-
-    if we don't find one: return Normal
-*)
-
-type EvalResult = Normal | Next of Exp
+type Result = Normal | Next of Exp
 
 let rec reduce = function
-    | Var v -> Normal
-    | App (Lam (p, x), a) ->
-        Next (subst (a, p, x))
-    | App (f, a) ->
-        match reduce f with
-            | Next nf -> Next (App (nf, a))
-            | Normal ->
-                match reduce a with
-                    | Next na -> Next (App (f, na))
-                    | Normal -> Normal
-    | Lam (p, x) ->
-         match reduce x with
-            | Next nx -> Next (Lam (p, nx))
-            | Normal -> Normal
-
-match reduce appExp with Next x -> unparse x
-
-(*
-    runEval (x: Exp) : unit
-    (Exp -> unit)
-    call reduce until it returns Normal
-    print intermediate results (the Next bits)
-*)
+| App (Lam (p, x), a) -> Next (subst (a, p, x))
+| Var v -> Normal
+| Lam (p, x) ->
+    match reduce x with
+    | Normal -> Normal
+    | Next nx -> Next (Lam (p, nx))
+| App (f, a) ->
+    match reduce f with
+    | Next nf -> Next (App (nf, a))
+    | Normal ->
+        match reduce a with
+        | Next na -> Next (App (f, na))
+        | Normal -> Normal
 
 let rec runEval x =
     match reduce x with
-        | Next nx ->
-            Console.WriteLine (unparse nx)
-            runEval nx
-        | Normal -> ()
+    | Normal -> ()
+    | Next nx ->
+        System.Console.WriteLine (unparse nx)
+        runEval nx
 
-runEval (appExp)
-runEval (lamExp1)
+let rec repl _ : unit =
+    runEval (parse expParser (System.Console.ReadLine ()))
+    repl ()
 
+(*
+(λx.λy.y) foo bar
+
+0
+λf.λx.x
+
+2
+λf.λx.f (f x)
+
+3
+λf.λx.f (f (f x))
+
+λa.λb.λf.λx.a f (b f x)
+
+(λa.λb.λf.λx.a f (b f x)) (λf.λx.f (f x)) (λf.λx.f (f (f x)))
+
+*)
 
